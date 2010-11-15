e2c9f0a4fb654eed61f99b5799530da9cb3d5d15
Stefan Oestreicher <stefan.oestreicher@gmail.com>
2010-11-15 14:12:46 +0100

    implement tutorial1 docking

--- src/com/vlsolutions/swing/examples/Tutorial1.java
+++ src/com/vlsolutions/swing/examples/Tutorial1.java
@@ -1,6 +1,7 @@
 package com.vlsolutions.swing.examples;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 
@@ -14,18 +15,26 @@ import javax.swing.JTree;
 import javax.swing.SwingUtilities;
 import javax.swing.table.DefaultTableModel;
 
+import com.vlsolutions.swing.docking.DockKey;
+import com.vlsolutions.swing.docking.Dockable;
+import com.vlsolutions.swing.docking.DockingConstants;
+import com.vlsolutions.swing.docking.DockingDesktop;
+
 public class Tutorial1 extends JFrame {
 	MyTextEditor editorPanel = new MyTextEditor();
 	MyTree treePanel = new MyTree();
 	MyGridOfButtons buttonGrid = new MyGridOfButtons();
 	MyJTable tablePanel = new MyJTable();
+	DockingDesktop desk = new DockingDesktop();
 
 	public Tutorial1() {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		getContentPane().add(editorPanel, BorderLayout.CENTER);
-		getContentPane().add(treePanel, BorderLayout.WEST);
-		getContentPane().add(buttonGrid, BorderLayout.NORTH);
-		getContentPane().add(tablePanel, BorderLayout.EAST);
+		getContentPane().add(desk);
+
+		desk.addDockable(editorPanel);
+		desk.split(editorPanel, treePanel, DockingConstants.SPLIT_LEFT);
+		desk.split(editorPanel, buttonGrid, DockingConstants.SPLIT_RIGHT);
+		desk.split(buttonGrid, tablePanel, DockingConstants.SPLIT_BOTTOM);
 	}
 
 	public static void main(String[] args) {
@@ -42,9 +51,9 @@ public class Tutorial1 extends JFrame {
 	}
 }
 
-class MyJTable extends JPanel {
+class MyJTable extends JPanel implements Dockable {
 	JTable table = new JTable();
-
+	DockKey key = new DockKey("table");
 	public MyJTable() {
 		setLayout(new BorderLayout());
 		table.setModel(new DefaultTableModel(5, 5));
@@ -52,9 +61,20 @@ class MyJTable extends JPanel {
 		jsp.setPreferredSize(new Dimension(200, 200));
 		add(jsp, BorderLayout.CENTER);
 	}
+
+	@Override
+	public DockKey getDockKey() {
+		return key;
+	}
+
+	@Override
+	public Component getComponent() {
+		return this;
+	}
 }
 
-class MyGridOfButtons extends JPanel {
+class MyGridOfButtons extends JPanel implements Dockable {
+	DockKey key = new DockKey("gridOfButtons");
 	public MyGridOfButtons() {
 		setLayout(new FlowLayout(FlowLayout.TRAILING, 3, 3));
 		for (int i = 0; i < 3; i++) {
@@ -64,26 +84,56 @@ class MyGridOfButtons extends JPanel {
 		}
 		setPreferredSize(new Dimension(200, 300));
 	}
+
+	@Override
+	public DockKey getDockKey() {
+		return key;
+	}
+
+	@Override
+	public Component getComponent() {
+		return this;
+	}
 }
 
-class MyTree extends JPanel {
+class MyTree extends JPanel implements Dockable {
 	JTree tree = new JTree();
-
+	DockKey key = new DockKey("tree");
 	public MyTree() {
 		setLayout(new BorderLayout());
 		JScrollPane jsp = new JScrollPane(tree);
 		jsp.setPreferredSize(new Dimension(200, 200));
 		add(jsp, BorderLayout.CENTER);
 	}
+
+	@Override
+	public DockKey getDockKey() {
+		return key;
+	}
+
+	@Override
+	public Component getComponent() {
+		return this;
+	}
 }
 
-class MyTextEditor extends JPanel {
+class MyTextEditor extends JPanel implements Dockable {
 	JTextArea textArea = new JTextArea("A Text Area");
-
+	DockKey key = new DockKey("textEditor");
 	public MyTextEditor() {
 		setLayout(new BorderLayout());
 		JScrollPane jsp = new JScrollPane(textArea);
 		jsp.setPreferredSize(new Dimension(300, 400));
 		add(jsp, BorderLayout.CENTER);
+	}
+
+	@Override
+	public DockKey getDockKey() {
+		return key;
+	}
+
+	@Override
+	public Component getComponent() {
+		return this;
 	}
 }
